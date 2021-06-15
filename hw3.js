d41fba64402c567247393e18fc19a4f2c6a963e4
Andrew Buchholz <learningemail33@gmail.com>
2021-06-15 16:25:15 -0400

    initial commit

---
+++ hw3.js
@@ -0,0 +1,45 @@
+//2
+
+for( var i = 0; i<11; i++){
+    console.log(i);
+}
+
+
+//3-4
+
+while(true){
+    console.log(true);
+    break;
+}
+
+//5-6
+
+for(var i=99; i>0; i--){
+    console.log(i + ' bottles of beer on the wall. Take one down, pass it around.');
+
+}
+
+//7-8
+
+var boolz = ['true', 'true', 'false', 'true', 'false', 'true', 'true', 'true'];
+
+
+
+for(var i = 0; i< boolz.length; i++){
+    if(i == true){
+        console.log(i)
+    }        
+}
+
+//9-10
+
+var str = 'apples';
+
+for (var i = 0; i< str.length; i++) {
+    if(i == 'a' || 'e'){
+        console.log(i);
+    }
+
+}
+
+
